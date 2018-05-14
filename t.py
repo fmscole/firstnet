a40fd72ef91579909d2a1a9a05d05c2e7e973eeb
fmscole <1047170189@qq.com>
2018-05-14 09:05:33 +0800

    sss

--- t.py
+++ t.py
@@ -1,53 +1,37 @@
-#coding=utf-8
-
-print("0")
+# from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-print("1")
-# import tensorflow.examples.tutorials.mnist.input_data
-# 加载数据
 # mnist = input_data.read_data_sets(r'./mnist', one_hot=True)
-"""
-# 创建模型
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-y = tf.matmul(x, W) + b
-"""
-print("2")
 
 import struct
 from glob import glob
 import os
-import numpy as np 
-
-print("2")
+import numpy as np
 def load_mnist(path, kind='train'):
     """Load MNIST data from `path`"""
     images_path = glob('./%s/%s*3-ubyte' % (path, kind))[0]
     labels_path = glob('./%s/%s*1-ubyte' % (path, kind))[0]
-    print(images_path,images_path)
+    print(images_path, images_path)
     with open(labels_path, 'rb') as lbpath:
         magic, n = struct.unpack('>II',
                                  lbpath.read(8))
-        
+
         labels = np.fromfile(lbpath,
                              dtype=np.uint8)
-        x=np.zeros((labels.shape[0],10))
+        x = np.zeros((labels.shape[0], 10))
         for i in range(labels.shape[0]):
-            x[i][labels[i]]=1
-        labels=np.array(x)
-        
+            x[i][labels[i]] = 1
+        labels = np.array(x)
     with open(images_path, 'rb') as imgpath:
         magic, num, rows, cols = struct.unpack('>IIII',
                                                imgpath.read(16))
         images = np.fromfile(imgpath,
                              dtype=np.uint8).reshape(len(labels), 784)
+        images=np.array(images)/255
         print(images.shape)
-        images=images/255
     return images, labels
-
 images, labels = load_mnist('./mnist')
 test_images, test_labels = load_mnist('./mnist', 't10k')
+
 
 x = tf.placeholder(tf.float32, [None, 784])
 W1 = tf.Variable(tf.truncated_normal([784, 500], stddev=0.1))
@@ -67,16 +51,18 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-print("now")
+batch_size=100
 # 训练过程
-batch_size=10
-for i in range(5000):
-    batch_xs=images[i * batch_size:(i + 1) * batch_size]
-    batch_ys = labels[i * batch_size:(i + 1) * batch_size]
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    if i %100 == 0:
+for i in range(600):
+    # batch_xs, batch_ys = mnist.train.next_batch(100)
+
+    batch_xs1 = images[i * batch_size:(i + 1) * batch_size]
+    batch_ys1 = labels[i * batch_size:(i + 1) * batch_size]
+    sess.run(train_step, feed_dict={x: batch_xs1, y_: batch_ys1})
+    if i%10 == 0:
         # 使用测试集评估准确率
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print (sess.run(accuracy, feed_dict = {x: test_images,
-                                                  y_: test_labels}))
+        # print (sess.run(accuracy, feed_dict = {x: mnist.test.images,
+        #                                           y_: mnist.test.labels}))
+        print (sess.run(accuracy, feed_dict = {x: test_images,y_: test_labels}))
